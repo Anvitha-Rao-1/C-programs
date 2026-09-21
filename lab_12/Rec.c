668200e00044777c0f57400acead0dc5f580ad38
Anvitha <Anvitha@gmail.com>
2026-09-21 19:54:21 +0530

    Rec lab

--- lab_12/Rec.c
+++ lab_12/Rec.c
@@ -1,108 +1,86 @@
 #include<stdio.h>
 
-int r1=1;
-int n4;
-int r4=1;
+int ans=1;
 
-/* 1. With reference variable concept */
-void fact1(int n)
+void calculate1(int num,int *ans)
 {
-    if(n>0)
+    if(num>0)
     {
-        r1=r1*n;
-        fact1(n-1);
+        printf("%d ",num);
+        *ans=*ans*num;
+        calculate1(num-1,ans);
     }
 }
 
-/* 2. No return type with parameter */
-void fact2(int n,int r)
+void calculate2(int num,int value)
 {
-    if(n>0)
+    if(num>0)
     {
-        r=r*n;
-        fact2(n-1,r);
+        value=value*num;
+        calculate2(num-1,value);
     }
     else
     {
-        printf("Factorial = %d\n",r);
+        printf("Factorial = %d\n",value);
     }
 }
 
-/* 3. With return type and parameter */
-int fact3(int n)
+int calculate3(int num)
 {
-    if(n<=1)
-        return 1;
-
-    return n*fact3(n-1);
-}
-
-/* 4. No return type and no parameter */
-void fact4()
-{
-    if(n4>0)
+    if(num<=1)
     {
-        r4=r4*n4;
-        n4--;
-        fact4();
+        return 1;
     }
+
+    return num*calculate3(num-1);
+}
+
+int count=5;
+int ans4=1;
+
+void calculate4()
+{
+    if(count>0)
+    {
+        ans4=ans4*count;
+        count--;
+        calculate4();
+    }
+}
+
+/* Extra recursion: Sum of numbers */
+int sum(int num)
+{
+    if(num<=0)
+    {
+        return 0;
+    }
+
+    return num+sum(num-1);
 }
 
 int main()
 {
-    int n,r3;
+    int fact=1;
 
-    printf("====================================\n");
-    printf("       FACTORIAL USING 4 METHODS\n");
-    printf("====================================\n");
+    printf("Method 1:\n");
+    calculate1(5,&fact);
+    printf("\nFactorial = %d\n",fact);
 
-    printf("Enter a number (0-12): ");
+    printf("\nMethod 2:\n");
+    calculate2(5,1);
 
-    if(scanf("%d",&n)!=1)
-    {
-        printf("Error: Invalid input!\n");
-        return 0;
-    }
+    printf("\nMethod 3:\n");
+    int answer=calculate3(5);
+    printf("Factorial = %d\n",answer);
 
-    if(n<0||n>12)
-    {
-        printf("Error: Enter a number between 0 and 12.\n");
-        return 0;
-    }
+    printf("\nMethod 4:\n");
+    calculate4();
+    printf("Factorial = %d\n",ans4);
 
-    printf("\n------------------------------------\n");
-    printf("1. WITH REFERENCE VARIABLE\n");
-    printf("------------------------------------\n");
-
-    fact1(n);
-    printf("Factorial = %d\n",r1);
-
-    printf("\n------------------------------------\n");
-    printf("2. NO RETURN TYPE + PARAMETER\n");
-    printf("------------------------------------\n");
-
-    fact2(n,1);
-
-    printf("\n------------------------------------\n");
-    printf("3. RETURN TYPE + PARAMETER\n");
-    printf("------------------------------------\n");
-
-    r3=fact3(n);
-    printf("Factorial = %d\n",r3);
-
-    printf("\n------------------------------------\n");
-    printf("4. NO RETURN TYPE + NO PARAMETER\n");
-    printf("------------------------------------\n");
-
-    n4=n;
-    r4=1;
-    fact4();
-
-    printf("Factorial = %d\n",r4);
-
-    printf("\n====================================\n");
-    printf("          PROGRAM COMPLETE\n");
-    printf("====================================\n");
+    printf("\nSum using Recursion:\n");
+    int total=sum(5);
+    printf("Sum = %d\n",total);
 
     return 0;
 }
